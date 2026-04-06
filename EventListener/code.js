7143165f7d51e84bd034a73ecbcf44e268352e25
Michi <71011437+Gen67@users.noreply.github.com>
2026-04-06 19:51:50 +0800

    Add files via upload

--- EventListener/code.js
+++ EventListener/code.js
@@ -1,6 +1,7 @@
 const box = document.getElementById("box");
 
 box.addEventListener("click", clickBox);
+box.addEventListener("mouseover", clickBox2);
 
 
 
@@ -9,3 +10,12 @@ function clickBox(event){
     box.querySelector("h1");
     box.textContent = "I was Clicked!";
 }
+
+
+function clickBox2(event){
+    box.style.backgroundColor = "yellow";
+    box.querySelector("h1");
+    box.textContent = "Don't Touch Me!";
+}
+
+
