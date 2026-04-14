acf65a6a311d0f43a689a57ce866471c22bb59d1
Michi <71011437+Gen67@users.noreply.github.com>
2026-04-14 14:07:20 +0800

    Add files via upload

---
+++ Reminder/script.js
@@ -0,0 +1,37 @@
+const addReminder = document.querySelector('.reminder-input'); 
+const reminderInput = document.querySelector('.reminder-form');
+const reminderList = document.querySelector('.reminder-items');
+const counter = document.querySelector('.counter');
+const clearBTN = document.getElementById('delete-btn');
+
+
+
+reminderInput.addEventListener('submit', event =>{
+    event.preventDefault();
+
+    const reminderText = addReminder.value.trim();
+
+    if(addReminder.value.trim() !== ''){
+       const addItem = document.createElement('p');
+
+
+        addItem.textContent = `~${reminderText}`;
+        addItem.classList.add('reminder-item');
+        reminderList.appendChild(addItem);
+        addReminder.value = '';
+
+
+        counter.style = 'display: block';
+        counter.textContent = reminderList.children.length;
+    } else{ 
+        alert('Please enter a reminder');
+    }
+
+    
+
+}); 
+
+clearBTN.addEventListener('click', () => {
+    reminderList.innerHTML = '';
+    counter.style = 'display: none';
+});
